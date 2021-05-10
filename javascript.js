fc1494e916d5379465ded178e2d5ccf4433b93f5
dropface12 <jafalquez@unirnote.edu.co>
2021-05-10 03:37:17 -0500

    cree

---
+++ javascript.js
@@ -0,0 +1,32 @@
+function vertificarplaca() {
+
+
+  const regex = /^([A-Z]{3})([0-9]{3})$/;
+  string=document.getElementById('placa').value
+  const isExisting = regex.test(string);
+  jlabel(isExisting,"placa")
+
+}
+
+function vertificarfecha() {
+
+  const regex = /^(0[1-9]|[1-2][0-9]|3[0-1])\/(1[0-2]|[1-9])\/([0-9]{4})$/;
+  string=document.getElementById('fecha').value
+  const isExisting = regex.test(string);
+  jlabel(isExisting,"fecha")
+}
+
+  function jlabel(isExisting,elemento){
+    elemento="es"+elemento
+    noelemento="no"+elemento
+    console.log(noelemento)
+    if(isExisting){
+      document.getElementById(elemento).style.display="block";
+      document.getElementById(elemento).style.color="green";
+      document.getElementById(noelemento).style.display="none";         
+  }else{
+      document.getElementById(noelemento).style.display="block";         
+      document.getElementById(noelemento).style.color="red";
+      document.getElementById(elemento).style.display="none"; 
+  }
+}
